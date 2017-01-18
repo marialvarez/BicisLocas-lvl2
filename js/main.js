9113bbb14ba744454ab3989ca70c5fa9c2c182fb
Maricela Alvarez <malvarezdrobush@gmail.com>
2017-01-18 12:48:11 -0500

    Trabajo terminado

--- js/main.js
+++ js/main.js
@@ -8,20 +8,39 @@
 //*NOTA: * Recomendamos que el mensaje se añada con un span ya que los estilos ya están definidos.
 
 function mensaje(campo,texto){
-    /*if(campo.nextSibling != null || campo.nextSibling == element.tagName("span")) {
-        campo.nextSibling.innerHTML = texto.textContent;
-    } else { }*/
-    
+    console.log(campo.nextSibling);
+    if(campo.nextSibling != null) {
+        if(campo.nextSibling.tagName == 'SPAN'){
+           campo.nextSibling.innerHTML = texto;
+        } else {
+            //Creando la Caja Negra
+            var cajaNegra = document.createElement("span");
+            //Crear nodo texto
+            var info = document.createTextNode(texto);
+            //Insertar el nodo Texto dentro del nodo Caja Negra
+            cajaNegra.appendChild(info);
+            //Crear padre
+            var padre = campo.parentNode;
+
+            var resultado = padre.appendChild(cajaNegra);
+            return resultado;    
+        }
+    } else { 
         //Creando la Caja Negra
         var cajaNegra = document.createElement("span");
+        //Crear nodo texto
+        var info = document.createTextNode(texto);
         //Insertar el nodo Texto dentro del nodo Caja Negra
-        cajaNegra.appendChild(texto);
+        cajaNegra.appendChild(info);
         //Crear padre
         var padre = campo.parentNode;
 
         var resultado = padre.appendChild(cajaNegra);
         return resultado;  
 
+    }
+    
+        
 }
 
 
@@ -30,20 +49,51 @@ function validateName(_evt){
     //Input
     var nombre = document.getElementById("name");
     //Creando el texto de la Caja Negra
-    var textoNombre = document.createTextNode("Debe ingresar su nombre");
-    var textoNumero = document.createTextNode("Los números no son válidos");
-    
+    var textoNombre = "Debe ingresar su nombre";
+    var textoNumero = "Los números no son válidos";
  
-    if(nombre.value==""){
-       mensaje(nombre,textoNombre); 
-    } else {
+    
+    if(nombre.value!=""){
+        //MAYUSCULA
+        var nombreArray = nombre.value.split("");
+        var primeraLetra = nombreArray[0];
+        var mayuscula = primeraLetra.toUpperCase();
+        var espacio = false;
+        
+        for(var i=1; i<nombreArray.length; i++) {
+            if(espacio){
+                mayuscula += nombreArray[i].toUpperCase();
+                espacio = false;
+            } else {
+                mayuscula += nombreArray[i];
+                if(nombreArray[i] == " ")
+                    espacio = true;
+            }
+        nombre.value = mayuscula;
+        }
+        
         if(/([0-9])/g.test(nombre.value)){
             mensaje(nombre,textoNumero);
         } else {
             nombre.parentNode.removeChild(nombre.nextSibling);
         }
+    } else {
+        mensaje(nombre,textoNombre);
+    }    
+}
+
+function validateLastname(_evt) {
+    
+    //Input
+    var apellido = document.getElementById("lastname");
+    //Creando el texto de la Caja Negra
+    var textoApellido = "Debe ingresar su apellido";
+    var textoNumero = "Los números no son válidos";
+    
+ 
+    if(apellido.value!=""){
         //MAYUSCULA
-        var nombreArray = nombre.value.split("");
+        var nombreArray = apellido.value.split("");
         var primeraLetra = nombreArray[0];
         var mayuscula = primeraLetra.toUpperCase();
         var espacio = false;
@@ -57,60 +107,30 @@ function validateName(_evt){
                 if(nombreArray[i] == " ")
                     espacio = true;
             }
-        nombre.value = mayuscula;
+        apellido.value = mayuscula;
         }
-    }
-}
-
-function validateLastname(_evt) {
-    
-    //Input
-    var apellido = document.getElementById("lastname");
-    //Creando el texto de la Caja Negra
-    var textoApellido = document.createTextNode("Debe ingresar su apellido");
-    var textoNumero = document.createTextNode("Los números no son válidos");
-    
- 
-    if(apellido.value==""){
-       mensaje(apellido,textoApellido); 
-    } else {
         if(/([0-9])/g.test(apellido.value)){
             mensaje(apellido,textoNumero)
         } else {
             apellido.parentNode.removeChild(apellido.nextSibling);
         }
-        //MAYUSCULA
-            var nombreArray = apellido.value.split("");
-            var primeraLetra = nombreArray[0];
-            var mayuscula = primeraLetra.toUpperCase();
-            var espacio = false;
-
-            for(var i=1; i<nombreArray.length; i++) {
-                if(espacio){
-                    mayuscula += nombreArray[i].toUpperCase();
-                    espacio = false;
-                } else {
-                    mayuscula += nombreArray[i];
-                    if(nombreArray[i] == " ")
-                        espacio = true;
-                }
-            apellido.value = mayuscula;
-            }
-    } 
+    } else {
+        mensaje(apellido,textoApellido); 
+    }
 }
 
 function validateEmail(_evt){
     
     //Input
-    var email = document.getElementById("input-email");
+    var emailX = document.getElementById("input-email");
     //Creando el texto de la Caja Negra
-    var textoEmail = document.createTextNode("Verifique su e-mail");
-    
-    if(!/([a-zA-Z0-9(-_.)]+[@][a-zA-Z0-9]+[.][a-zA-Z]+)/g.test(email.value)){
-        mensaje(email,textoEmail);
+    var textoEmail = "Verifique su e-mail";
+
+    if(/([a-zA-Z0-9(-_.)]+[@][a-zA-Z0-9]+[.][a-zA-Z]+)/g.test(emailX.value)){
+        emailX.parentNode.removeChild(emailX.nextSibling); 
     } else {
-        if(email.value.length>0){
-            email.parentNode.removeChild(email.nextSibling);  
+        if(emailX.value.length > 0){
+            mensaje(emailX,textoEmail);  
         }
     }
 }
@@ -120,13 +140,15 @@ function validatePassword(_evt) {
     //Input
     var password = document.getElementById("input-password");
     //Creando el texto de la Caja Negra
-    var textoPassword = document.createTextNode("La contraseña debe tener al menos 6 caracteres");
+    var textoPassword = "La contraseña debe tener al menos 6 caracteres";
     
-    if((password.value.length < 6) ) {
-       mensaje(password,textoPassword);
-      } else {
-       password.parentNode.removeChild(password.nextSibling);
-      }
+    if(/([1-6])/g.test(password.value) || (/([9]+[8]+[7]+[5]+[4])/g.test(password.value)) || password.value.length < 6) {
+        mensaje(password,textoPassword); 
+        
+    } else {
+        password.parentNode.removeChild(password.nextSibling);  
+       
+    }
 }
 
 function validateType(_evt) {
@@ -134,27 +156,30 @@ function validateType(_evt) {
     //Input
     var tipo = document.querySelector("select");
     //Creando el texto de la Caja Negra
-    var textoTipo = document.createTextNode("Debes seleccionar al menos un tipo de bici");
+    var textoTipo = "Debes seleccionar al menos un tipo de bici";
     
     if(tipo.value == 0){
           mensaje(tipo,textoTipo);
-      } else {
-          tipo.parentNode.removeChild(tipo.nextSibling);
-      }   
+    } else { 
+        if(tipo.value == "urbana" || tipo.value == "treking" || tipo.value == "electrica" || tipo.value == "estatica"){
+           tipo.parentNode.removeChild(tipo.nextSibling); 
+        }
+          
+    }   
 }
 
 function validateForm(){
-    /*var name = document.getElementById("name");
+    var name = document.getElementById("name");
     var lastname = document.getElementById("lastname");
     var email = document.getElementById("input-email");
     var password = document.getElementById("input-password");
     var type = document.querySelector("select");
     
-    var textoNombre = document.createTextNode("Debe ingresar su nombre");
-    var textoApellido = document.createTextNode("Debe ingresar su apellido");
-    var textoEmail = document.createTextNode("Verifique su e-mail");
-    var textoPassword = document.createTextNode("La contraseña debe tener al menos 6 caracteres");
-    var textoTipo = document.createTextNode("Debes seleccionar al menos un tipo de bici");
+    var textoNombre = "Debe ingresar su nombre";
+    var textoApellido = "Debe ingresar su apellido";
+    var textoEmail = "Verifique su e-mail";
+    var textoPassword = "La contraseña debe tener al menos 6 caracteres";
+    var textoTipo = "Debes seleccionar al menos un tipo de bici";
     
     if(name.value.length==0 && lastname.value.length==0 && email.value.length==0 && password.value.length==0 && type.value==0) {
         mensaje(name,textoNombre);
@@ -162,11 +187,13 @@ function validateForm(){
         mensaje(email,textoEmail);
         mensaje(password,textoPassword);
         mensaje(type,textoTipo);
-    }*/
+    } else {
+        
+    }
     
-    validateName();
+    /*validateName();
     validateLastname();
     validateEmail();
     validatePassword();
-    validateType();
+    validateType();*/
 }
